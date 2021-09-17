72538cffd3b0eef1443a64dc9c68c76cd4a96206
Yiğit Ö. Ünver <69584310+younver@users.noreply.github.com>
2021-09-18 00:27:50 +0300

    Update mini_max_sum.cpp

--- mini_max_sum.cpp
+++ mini_max_sum.cpp
@@ -5,10 +5,10 @@
  */
 
 void miniMaxSum(vector<int> arr) {
-    int64_t max = 0, min = 0, sum=0;
+    int64_t max = arr[i], min = arr[i], sum=arr[i];
 
     // Input shape => Five positive integers 
-    for(int i=0; i<5; i++)
+    for(int i=1; i<5; i++)
     {
         sum+=arr[i];
         if(arr[i] > max) max = arr[i];
