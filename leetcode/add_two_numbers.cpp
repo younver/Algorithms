ad62f20d7cf2be76a01e3c26b9df5e485c5f39a9
Yiğit Ö. Ünver <69584310+younver@users.noreply.github.com>
2021-10-04 22:41:32 +0300

    Add Two Numbers
    C++ | Solution object for "Add Two Numbers" from LeetCode.

---
+++ leetcode/add_two_numbers.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        int carry = 0;
+        ListNode head; // just 0 initially
+        ListNode* tail = &head; // pointer tail => address of head
+
+        while (carry || l1 || l2) {
+            std::cout << l1->val << " AND " << l2->val << std::endl;
+
+            if (l1) {
+                carry += l1->val;
+                l1 = l1->next;
+            }
+
+            if (l2) {
+                carry += l2->val;
+                l2 = l2->next;
+            }
+            tail->next = new ListNode(carry % 10);
+            tail = tail->next;
+            carry /= 10;
+        }
+        return head.next;
+    }
+};
